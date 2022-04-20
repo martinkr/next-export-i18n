d6dccaec14177d3efd2f574bcb626886e4205866
Bittmann Christoph (IT-PTR-EXT - Extern) <christoph.bittmann@sbb.ch>
2022-04-20 09:36:43 +0200

    fix: formatting

--- module/src/hooks/use-language-query.test.tsx
+++ module/src/hooks/use-language-query.test.tsx
@@ -1,121 +1,121 @@
 /**
  * @jest-environment jsdom
  */
-// import { renderHook, act } from '@testing-library/react-hooks';
 import { cleanup, renderHook } from '@testing-library/react';
 import useLanguageQuery from './use-language-query';
 
+
 jest.mock('./../../../i18n/index', () => {
-  return {
-    __esModule: true,
-    i18n: {
-      translations: { mock: { title: 'mock' }, foo: { title: 'bar' } },
-      defaultLang: 'mock',
-    },
-  };
+	return {
+		__esModule: true,
+		i18n: {
+			translations: { mock: { title: 'mock' }, foo: { title: 'bar' } },
+			defaultLang: 'mock',
+		},
+	};
 });
 
 jest.mock('next/router', () => ({
-  useRouter() {
-    return {
-      route: '/',
-      pathname: '',
-      query: '',
-      asPath: '',
-    };
-  },
+	useRouter() {
+		return {
+			route: '/',
+			pathname: '',
+			query: '',
+			asPath: '',
+		};
+	},
 }));
 const useRouter = jest.spyOn(require('next/router'), 'useRouter');
 
 jest.mock('./use-selected-language', () => {
-  return {
-    __esModule: true,
-    default: () => {},
-  };
+	return {
+		__esModule: true,
+		default: () => { },
+	};
 });
 
 const useSelectedLanguage = jest.spyOn(
-  require('./use-selected-language'),
-  'default'
+	require('./use-selected-language'),
+	'default'
 );
 
 beforeEach(() => {
-  useSelectedLanguage.mockImplementation(() => ({
-    lang: 'mock',
-  }));
+	useSelectedLanguage.mockImplementation(() => ({
+		lang: 'mock',
+	}));
 });
 
 afterEach(() => {
-  cleanup();
-  jest.clearAllMocks();
+	cleanup();
+	jest.clearAllMocks();
 });
 
 describe('The hook returns ', () => {
-  it(`the query object with lang = forceLang if forceLang is passed `, async () => {
-    const expectation = [
-      {
-        bar: 'baz',
-        lang: 'forced',
-      },
-    ];
-    useRouter.mockImplementation(() => ({
-      query: { bar: 'baz', lang: 'foo' },
-    }));
-    useSelectedLanguage.mockImplementation(() => ({
-      lang: 'bar',
-    }));
+	it(`the query object with lang = forceLang if forceLang is passed `, async () => {
+		const expectation = [
+			{
+				bar: 'baz',
+				lang: 'forced',
+			},
+		];
+		useRouter.mockImplementation(() => ({
+			query: { bar: 'baz', lang: 'foo' },
+		}));
+		useSelectedLanguage.mockImplementation(() => ({
+			lang: 'bar',
+		}));
 
-    const { result } = renderHook(() => useLanguageQuery('forced'));
-    expect(result.current).toEqual(expectation);
-  });
+		const { result } = renderHook(() => useLanguageQuery('forced'));
+		expect(result.current).toEqual(expectation);
+	});
 
-  it(`the query object with lang = selectedLanguage if no forceLang is passed and selectedLanguage is present`, async () => {
-    const expectation = [
-      {
-        bar: 'baz',
-        lang: 'bar',
-      },
-    ];
-    useRouter.mockImplementation(() => ({
-      query: { bar: 'baz', lang: 'foo' },
-    }));
-    useSelectedLanguage.mockImplementation(() => ({
-      lang: 'bar',
-    }));
+	it(`the query object with lang = selectedLanguage if no forceLang is passed and selectedLanguage is present`, async () => {
+		const expectation = [
+			{
+				bar: 'baz',
+				lang: 'bar',
+			},
+		];
+		useRouter.mockImplementation(() => ({
+			query: { bar: 'baz', lang: 'foo' },
+		}));
+		useSelectedLanguage.mockImplementation(() => ({
+			lang: 'bar',
+		}));
 
-    const { result } = renderHook(() => useLanguageQuery());
-    expect(result.current).toEqual(expectation);
-  });
+		const { result } = renderHook(() => useLanguageQuery());
+		expect(result.current).toEqual(expectation);
+	});
 
-  it(`the query object with lang = lang if no forceLang is passed and no selectedLanguage is present`, async () => {
-    const expectation = [
-      {
-        bar: 'baz',
-        lang: 'foo',
-      },
-    ];
-    useRouter.mockImplementation(() => ({
-      query: { bar: 'baz', lang: 'foo' },
-    }));
-    useSelectedLanguage.mockImplementation(() => ({
-      lang: '',
-    }));
+	it(`the query object with lang = lang if no forceLang is passed and no selectedLanguage is present`, async () => {
+		const expectation = [
+			{
+				bar: 'baz',
+				lang: 'foo',
+			},
+		];
+		useRouter.mockImplementation(() => ({
+			query: { bar: 'baz', lang: 'foo' },
+		}));
+		useSelectedLanguage.mockImplementation(() => ({
+			lang: '',
+		}));
 
-    const { result } = renderHook(() => useLanguageQuery());
-    expect(result.current).toEqual(expectation);
-  });
+		const { result } = renderHook(() => useLanguageQuery());
+		expect(result.current).toEqual(expectation);
+	});
 
-  it(`an empty object if there is no query`, async () => {
-    const expectation = [{}];
-    useRouter.mockImplementation(() => ({
-      query: null,
-    }));
-    useSelectedLanguage.mockImplementation(() => ({
-      lang: '',
-    }));
+	it(`an empty object if there is no query`, async () => {
+		const expectation = [{}];
+		useRouter.mockImplementation(() => ({
+			query: null,
+		}));
+		useSelectedLanguage.mockImplementation(() => ({
+			lang: '',
+		}));
 
-    const { result } = renderHook(() => useLanguageQuery());
-    expect(result.current).toEqual(expectation);
-  });
+		const { result } = renderHook(() => useLanguageQuery());
+		expect(result.current).toEqual(expectation);
+	});
 });
 //
