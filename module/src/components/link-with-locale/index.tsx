5ceeee01e040649096bfc9fedacaf859284f90e4
Martin Krause <github@mkrause.info>
2023-12-08 18:41:54 +0200

    2.3.4 adds export for linkwithlocale

--- module/src/components/link-with-locale/index.tsx
+++ module/src/components/link-with-locale/index.tsx
@@ -1,20 +1,20 @@
-import Link, { LinkProps } from 'next/link';
-import useSelectedLanguage from '../../hooks/use-selected-language';
-import React, { ReactNode,useMemo } from 'react';
+import Link, { LinkProps } from "next/link";
+import useSelectedLanguage from "../../hooks/use-selected-language";
+import React, { ReactNode, useMemo } from "react";
 
 interface LinkWithLocaleProps extends LinkProps {
   children: ReactNode;
 }
 
-export function LinkWithLocale(props: LinkWithLocaleProps) {
+export default function LinkWithLocale(props: LinkWithLocaleProps) {
   const { lang } = useSelectedLanguage();
   const { href, ...rest } = props;
   const link = useMemo(() => {
     const inputHref = href.toString();
-    if(inputHref.includes("?lang=") || inputHref.includes("&lang=")){
-        return inputHref;
+    if (inputHref.includes("?lang=") || inputHref.includes("&lang=")) {
+      return inputHref;
     }
-    if (inputHref.includes('?')) {
+    if (inputHref.includes("?")) {
       return `${inputHref}&lang=${lang}`;
     } else {
       return `${inputHref}?lang=${lang}`;
--- module/src/export.tsx
+++ module/src/export.tsx
@@ -1,16 +1,23 @@
-
 /**
  * the unified entry file for the exported and compiled node module
  */
 import { i18n as userland } from "./../../i18n/index.js";
-import useLanguageQuery from './hooks/use-language-query';
-import useSelectedLanguage from './hooks/use-selected-language';
-import useLanguageSwitcherIsActive from './hooks/use-language-switcher-is-active';
-import { useTranslation } from './hooks/use-translation';
-import LanguageSwitcher from './components/language-switcher';
+import useLanguageQuery from "./hooks/use-language-query";
+import useSelectedLanguage from "./hooks/use-selected-language";
+import useLanguageSwitcherIsActive from "./hooks/use-language-switcher-is-active";
+import { useTranslation } from "./hooks/use-translation";
+import LanguageSwitcher from "./components/language-switcher";
+import LinkWithLocale from "./components/link-with-locale";
 import i18n from "./index.js";
 
-import { Dictionary, I18N } from './types';
+import { Dictionary, I18N } from "./types";
 
-export { useLanguageQuery, useSelectedLanguage, useLanguageSwitcherIsActive, useTranslation, LanguageSwitcher };
+export {
+  useLanguageQuery,
+  useSelectedLanguage,
+  useLanguageSwitcherIsActive,
+  useTranslation,
+  LanguageSwitcher,
+  LinkWithLocale,
+};
 export default i18n;
