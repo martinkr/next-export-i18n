e1cfb233736819c78d4c18cefdabc42a8b3a39a7
Martin Krause <github@mkrause.info>
2023-12-08 12:20:21 +0200

    updates export.tsx

--- module/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import useLanguageQuery from './hooks/use-language-query';
-import useSelectedLanguage from './hooks/use-selected-language';
-import useLanguageSwitcherIsActive from './hooks/use-language-switcher-is-active';
-import { useTranslation } from './hooks/use-translation';
-import LanguageSwitcher from './components/language-switcher';
-import i18n from "./index.js";
-export { useLanguageQuery, useSelectedLanguage, useLanguageSwitcherIsActive, useTranslation, LanguageSwitcher };
-export default i18n;
