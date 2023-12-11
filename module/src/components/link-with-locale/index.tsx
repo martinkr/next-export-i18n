6ceda788f15d10a37ce9e06b4a39cab1794eba08
Martin Krause <github@mkrause.info>
2023-12-11 14:38:00 +0200

    test for the linkwithlocale component

--- module/src/components/link-with-locale/index.tsx
+++ module/src/components/link-with-locale/index.tsx
@@ -11,6 +11,18 @@ interface LinkWithLocaleProps extends LinkProps {
   children: ReactNode;
 }
 
+/**
+ * Simple component wrapper for links with a locale. use it for internal links.
+ * Either add the lang parameter to the link target in the href-attribute to
+ * force a specific language (e.g. as a language switcher) or let the component
+ * add the currently selected languate automatically. Preserves the current query parameters
+ * @exmaple
+ *  <LinkWithLocale href={`${t("nav.en.about.href")}?share=social"}>
+ *    {t(nav.en.about.text")}
+ *  </LinkWithLocale>
+ * @param href string the value for the href-attribute
+ * @param [children] string the text to display
+ */
 export default function LinkWithLocale(props: LinkWithLocaleProps) {
   const { lang } = useSelectedLanguage();
   const i18nObj = i18n() as I18N;
